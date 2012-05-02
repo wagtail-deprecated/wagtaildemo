d5843b3d31134beac3bea6310e7b08f82497c9da
Matt Westcott <matt@west.co.tt>
2012-05-02 17:11:24 +0100

    virtualenv dir should be .virtualenvs, not .virtualenv

--- etc/puppet/modules/virtualenv/manifests/init.pp
+++ etc/puppet/modules/virtualenv/manifests/init.pp
@@ -1,7 +1,7 @@
 class virtualenv ($virtualenv_name) {
 	include python2
 	
-	$virtualenv_path = "/home/vagrant/.virtualenv/${virtualenv_name}"
+	$virtualenv_path = "/home/vagrant/.virtualenvs/${virtualenv_name}"
 	exec {"mkvirtualenv $virtualenv_name":
 		command => "/usr/local/bin/virtualenv --no-site-packages $virtualenv_path",
 		user => vagrant,
