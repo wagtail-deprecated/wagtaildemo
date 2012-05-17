caa22e74d7cc15a971b53c3e294ec162b3e1e20d
Matt Westcott <matt@west.co.tt>
2012-05-17 14:30:27 +0100

    add initial staticfiles / template dirs, enable devserver, add email subject prefix

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -1,5 +1,9 @@
 # Django settings for {{ project_name }} project.
 
+import os
+
+PROJECT_ROOT = os.path.join(os.path.dirname(__file__), '..', '..')
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -59,7 +63,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -70,6 +74,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, 'staticfiles'),
 )
 
 # List of finder classes that know how to find static files in
@@ -108,6 +113,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, 'templates'),
 )
 
 INSTALLED_APPS = (
@@ -119,11 +125,14 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
 
     'south',
+    'devserver',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
+
+EMAIL_SUBJECT_PREFIX = '[{ project_name }] '
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
