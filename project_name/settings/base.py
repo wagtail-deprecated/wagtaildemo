86648497da6326a86952b17e3852d807f1a1a578
Matt Westcott <matt@west.co.tt>
2012-05-31 14:24:06 +0100

    add core app

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -140,6 +140,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+
+    'core',
 )
 
 EMAIL_SUBJECT_PREFIX = '[{ project_name }] '
