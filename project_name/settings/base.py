2c9b2b979a65eb79d5eadc17bed389454fec8678
Matt Westcott <matt@west.co.tt>
2013-04-10 22:46:29 +0100

    enable admin by default

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -138,8 +138,7 @@ INSTALLED_APPS = (
     'compressor',
     'debug_toolbar',
 
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 
