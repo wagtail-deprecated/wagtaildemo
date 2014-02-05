585351ec1eab7897bfae162e24af0c65389f2e61
Helen C <helen.chapman@torchbox.com>
2014-02-05 11:20:36 +0000

    fixed path for images

--- wagtaildemo/settings/base.py
+++ wagtaildemo/settings/base.py
@@ -60,12 +60,12 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
