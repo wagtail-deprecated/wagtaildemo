6abda07d4d7d218ce5424017c0b616881e392ee6
Dave Cranwell <xcession@xcessionsites.com>
2014-02-19 11:26:40 +0000

    Added warning about return from function

--- demo/templatetags/demo_tags.py
+++ demo/templatetags/demo_tags.py
@@ -14,6 +14,8 @@ def get_googe_maps_key():
 
 @register.assignment_tag(takes_context=True)
 def get_site_root(context):
+    # NB this returns a core.Page, not the implementation-specific model used
+    # so object-comparison to self will return false as objects would differ
     return context['request'].site.root_page
 
 
