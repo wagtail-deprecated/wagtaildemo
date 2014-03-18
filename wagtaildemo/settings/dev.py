674e9e7e1d4ebca31661c032e66846098c75c936
Matt Westcott <matt@west.co.tt>
2014-03-18 17:33:49 +0000

    Remove django-debug-toolbar from default dev installation - it's not relevant to demoing Wagtail, confusing for people who aren't familiar with it, and easy enough for people to install for themselves if they want it

--- wagtaildemo/settings/dev.py
+++ wagtaildemo/settings/dev.py
@@ -7,19 +7,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # BASE_URL required for notification emails
 BASE_URL = 'http://localhost:8111'
 
-INSTALLED_APPS += (
-    'debug_toolbar',
-)
-
-MIDDLEWARE_CLASSES += (
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-)
-
-# django-debug-toolbar settings
-DEBUG_TOOLBAR_CONFIG = {
-    'INTERCEPT_REDIRECTS': False,
-}
-
 try:
 	from .local import *
 except ImportError:
