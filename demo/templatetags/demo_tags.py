1636576a3ada00d7e313479793d2ecdcf1db9b39
Bertrand Bordage <bordage.bertrand@gmail.com>
2015-02-05 15:34:47 +0100

    Fixes a bug with the navbar when "self" is not defined in context.
    It happens when we search something for example.

--- demo/templatetags/demo_tags.py
+++ demo/templatetags/demo_tags.py
@@ -33,8 +33,11 @@ def top_menu(context, parent, calling_page=None):
     menuitems = parent.get_children().live().in_menu()
     for menuitem in menuitems:
         menuitem.show_dropdown = has_menu_children(menuitem)
-        menuitem.active = (False if calling_page is None
-                           else calling_page.url.startswith(menuitem.url))
+        # We don't directly check if calling_page is None since the template
+        # engine can pass an empty string to calling_page
+        # if the variable passed as calling_page does not exist.
+        menuitem.active = (calling_page.url.startswith(menuitem.url)
+                           if calling_page else False)
     return {
         'calling_page': calling_page,
         'menuitems': menuitems,
