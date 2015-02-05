6a5bdc115623603885ec2d2bf73eedcd4e053b44
Bertrand Bordage <bordage.bertrand@gmail.com>
2015-02-05 15:33:21 +0100

    Explicits imports.
    Otherwise, it’s unclear if we import datetime.date from models or from the current module.

--- demo/templatetags/demo_tags.py
+++ demo/templatetags/demo_tags.py
@@ -2,7 +2,7 @@ from datetime import date
 from django import template
 from django.conf import settings
 
-from demo.models import *
+from demo.models import PersonPage, BlogPage, EventPage, Advert, Page
 
 register = template.Library()
 
