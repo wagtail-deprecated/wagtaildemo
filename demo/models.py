6759e7dfa428901861261b73590ca0e51b65447b
Matt Westcott <matt@west.co.tt>
2015-09-01 11:13:37 +0100

    PEP8 cleanup

--- demo/models.py
+++ demo/models.py
@@ -2,7 +2,6 @@ from datetime import date
 
 from django.db import models
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.shortcuts import render
 from django.http import HttpResponse
 
 from wagtail.wagtailcore.models import Page, Orderable
@@ -10,7 +9,6 @@ from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailadmin.edit_handlers import FieldPanel, MultiFieldPanel, \
     InlinePanel, PageChooserPanel
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
-from wagtail.wagtailimages.models import Image
 from wagtail.wagtaildocs.edit_handlers import DocumentChooserPanel
 from wagtail.wagtailsnippets.models import register_snippet
 from wagtail.wagtailforms.models import AbstractEmailForm, AbstractFormField
@@ -18,7 +16,7 @@ from wagtail.wagtailsearch import index
 
 from modelcluster.fields import ParentalKey
 from modelcluster.tags import ClusterTaggableManager
-from taggit.models import Tag, TaggedItemBase
+from taggit.models import TaggedItemBase
 
 from demo.utils import export_event
 
@@ -581,6 +579,7 @@ EventPage.promote_panels = Page.promote_panels + [
 
 class FormField(AbstractFormField):
     page = ParentalKey('FormPage', related_name='form_fields')
+
 
 class FormPage(AbstractEmailForm):
     intro = RichTextField(blank=True)
