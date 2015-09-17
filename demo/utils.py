9e3c8fe956ff40784aa127fb059d0c77fe4561a4
Karl Hobley <karlhobley10@gmail.com>
2015-09-17 15:18:30 +0100

    Added generate_image_url function

--- demo/utils.py
+++ demo/utils.py
@@ -1,6 +1,25 @@
 from datetime import datetime, time, timedelta
 import hashlib
 
+from django.core.urlresolvers import reverse
+
+from wagtail.wagtailcore.models import Site
+from wagtail.wagtailimages.utils import generate_signature
+
+
+def generate_image_url(image, filter_spec):
+    signature = generate_signature(image.id, filter_spec)
+    url = reverse('wagtailimages_serve', args=(signature, image.id, filter_spec))
+
+    try:
+        site_root_url = Site.objects.get(is_default_site=True).root_url
+    except Site.DoesNotExist:
+        site_root_url = Site.objects.first().root_url
+
+    image_filename = image.file.name[len('original_images/'):]
+
+    return site_root_url + url + image_filename
+
 
 def export_event(event, format='ical'):
     # Only ical format supported at the moment
