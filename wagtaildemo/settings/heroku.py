80783ee17b38c0e3629a1ba6f22f67150792f87a
Karl Hobley <karlhobley10@gmail.com>
2015-11-02 14:10:40 +0000

    Cleanup heroku settings

--- wagtaildemo/settings/heroku.py
+++ wagtaildemo/settings/heroku.py
@@ -1,5 +1,8 @@
-from .base import *
+import dj_database_url
 import os
+
+from .base import *
+
 
 DEBUG = True
 
@@ -8,8 +11,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # BASE_URL required for notification emails
 BASE_URL = 'http://localhost:8000'
 
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
 DATABASES = {'default': dj_database_url.config(default='postgres://postgres@localhost:5432/wagtaildemo')}
 
 
