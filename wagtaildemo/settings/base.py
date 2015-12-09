498ee163028677de80d23827499c5fde5eb718a1
Matt Westcott <matt@west.co.tt>
2015-12-09 15:04:15 +0000

    Fix TEMPLATE_CONTEXT_PROCESSORS setting for Django 1.9 (it's now a list, not a tuple)

--- wagtaildemo/settings/base.py
+++ wagtaildemo/settings/base.py
@@ -120,9 +120,9 @@ MIDDLEWARE_CLASSES = (
 )
 
 from django.conf import global_settings
-TEMPLATE_CONTEXT_PROCESSORS = global_settings.TEMPLATE_CONTEXT_PROCESSORS + (
+TEMPLATE_CONTEXT_PROCESSORS = list(global_settings.TEMPLATE_CONTEXT_PROCESSORS) + [
     'django.core.context_processors.request',
-)
+]
 
 ROOT_URLCONF = 'wagtaildemo.urls'
 
